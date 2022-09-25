8fe1b5882fd045e947176ea5b45bf7a15ac79beb
Djostit <djostit@mail.ru>
2022-09-25 22:41:32 +0500

    Big Update

---
+++ app/src/main/java/com/example/orderclothes/Order_check.java
@@ -0,0 +1,38 @@
+package com.example.orderclothes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Order_check extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_check);
+
+        Bundle arg = getIntent().getExtras();
+
+        TextView login = findViewById(R.id.login);
+        TextView password = findViewById(R.id.password);
+
+        login.setText(arg.get("login").toString());
+        password.setText(arg.get("password").toString());
+
+        TextView ur_gender = findViewById(R.id.ur_gender);
+
+        ur_gender.setText(arg.get("gender").toString());
+
+        TextView size = findViewById(R.id.size);
+
+        size.setText(arg.get("size").toString());
+
+        TextView type_of_clothes = findViewById(R.id.type_of_clothes);
+
+        type_of_clothes.setText(arg.get("type_cloth").toString());
+
+        TextView clothes = findViewById(R.id.clothes);
+
+        clothes.setText(arg.get("cloth").toString());
+    }
+}
--- app/src/main/java/com/example/orderclothes/Ordering.java
+++ app/src/main/java/com/example/orderclothes/Ordering.java
@@ -2,8 +2,9 @@ package com.example.orderclothes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.TypedArray;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
@@ -29,6 +30,13 @@ public class Ordering extends AppCompatActivity {
     Spinner Outerwear_male;
     Spinner Underwear_male;
     Spinner Shoes_male;
+
+    Spinner Headgear_female;
+    Spinner Outerwear_female;
+    Spinner Underwear_female;
+    Spinner Shoes_female;
+
+    Button Place_an_order;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,6 +81,20 @@ public class Ordering extends AppCompatActivity {
         Underwear_male.setVisibility(View.GONE);
         Shoes_male.setVisibility(View.GONE);
 
+        Headgear_female = findViewById(R.id.Headgear_female);
+        Outerwear_female = findViewById(R.id.Outerwear_female);
+        Underwear_female = findViewById(R.id.Underwear_female);
+        Shoes_female = findViewById(R.id.Shoes_female);
+
+        Headgear_female.setVisibility(View.GONE);
+        Outerwear_female.setVisibility(View.GONE);
+        Underwear_female.setVisibility(View.GONE);
+        Shoes_female.setVisibility(View.GONE);
+
+        Place_an_order = findViewById(R.id.Place_an_order);
+
+        Place_an_order.setVisibility(View.GONE);
+
     }
 
     public void ShowAddition(View view) {
@@ -101,53 +123,193 @@ public class Ordering extends AppCompatActivity {
     }
 
     public void GetTypeCloth(View view) {
-        if(OutTypeCloth(select_type.getSelectedItemPosition()).contains("Headgear")){
-            if(male.isChecked()){
-                Headgear_male.setVisibility(View.VISIBLE);
-                Outerwear_male.setVisibility(View.GONE);
-                Underwear_male.setVisibility(View.GONE);
-                Shoes_male.setVisibility(View.GONE);
-            }
-            else if(female.isChecked()){
+        switch (OutTypeCloth(select_type.getSelectedItemPosition(), 1)){
+            case "Headgear":
+                if(male.isChecked()){
+                    Headgear_male.setVisibility(View.VISIBLE);
+                    Outerwear_male.setVisibility(View.GONE);
+                    Underwear_male.setVisibility(View.GONE);
+                    Shoes_male.setVisibility(View.GONE);
 
-            }
-        }
-        else if(OutTypeCloth(select_type.getSelectedItemPosition()).contains("Outerwear")){
-            if(male.isChecked()){
-                Headgear_male.setVisibility(View.GONE);
-                Outerwear_male.setVisibility(View.VISIBLE);
-                Underwear_male.setVisibility(View.GONE);
-                Shoes_male.setVisibility(View.GONE);
-            }
-            else if(female.isChecked()){
+                    Headgear_female.setVisibility(View.GONE);
+                    Outerwear_female.setVisibility(View.GONE);
+                    Underwear_female.setVisibility(View.GONE);
+                    Shoes_female.setVisibility(View.GONE);
 
-            }
-        }
-        else if(OutTypeCloth(select_type.getSelectedItemPosition()).contains("Underwear")){
-            if(male.isChecked()){
-                Headgear_male.setVisibility(View.GONE);
-                Outerwear_male.setVisibility(View.GONE);
-                Underwear_male.setVisibility(View.VISIBLE);
-                Shoes_male.setVisibility(View.GONE);
-            }
-            else if(female.isChecked()){
+                }
+                else if(female.isChecked()){
+                    Headgear_female.setVisibility(View.VISIBLE);
+                    Outerwear_female.setVisibility(View.GONE);
+                    Underwear_female.setVisibility(View.GONE);
+                    Shoes_female.setVisibility(View.GONE);
 
-            }
-        }
-        else if(OutTypeCloth(select_type.getSelectedItemPosition()).contains("Shoes")){
-            if(male.isChecked()) {
-                Headgear_male.setVisibility(View.GONE);
-                Outerwear_male.setVisibility(View.GONE);
-                Underwear_male.setVisibility(View.GONE);
-                Shoes_male.setVisibility(View.VISIBLE);
-            }
-            else if(female.isChecked()){
+                    Headgear_male.setVisibility(View.GONE);
+                    Outerwear_male.setVisibility(View.GONE);
+                    Underwear_male.setVisibility(View.GONE);
+                    Shoes_male.setVisibility(View.GONE);
+                }
+                break;
+            case "Outerwear":
+                if(male.isChecked()){
+                    Headgear_male.setVisibility(View.GONE);
+                    Outerwear_male.setVisibility(View.VISIBLE);
+                    Underwear_male.setVisibility(View.GONE);
+                    Shoes_male.setVisibility(View.GONE);
 
-            }
+                    Headgear_female.setVisibility(View.GONE);
+                    Outerwear_female.setVisibility(View.GONE);
+                    Underwear_female.setVisibility(View.GONE);
+                    Shoes_female.setVisibility(View.GONE);
+                }
+                else if(female.isChecked()){
+                    Headgear_female.setVisibility(View.GONE);
+                    Outerwear_female.setVisibility(View.VISIBLE);
+                    Underwear_female.setVisibility(View.GONE);
+                    Shoes_female.setVisibility(View.GONE);
+
+                    Headgear_male.setVisibility(View.GONE);
+                    Outerwear_male.setVisibility(View.GONE);
+                    Underwear_male.setVisibility(View.GONE);
+                    Shoes_male.setVisibility(View.GONE);
+                }
+                break;
+            case "Underwear":
+                if(male.isChecked()){
+                    Headgear_male.setVisibility(View.GONE);
+                    Outerwear_male.setVisibility(View.GONE);
+                    Underwear_male.setVisibility(View.VISIBLE);
+                    Shoes_male.setVisibility(View.GONE);
+
+                    Headgear_female.setVisibility(View.GONE);
+                    Outerwear_female.setVisibility(View.GONE);
+                    Underwear_female.setVisibility(View.GONE);
+                    Shoes_female.setVisibility(View.GONE);
+                }
+                else if(female.isChecked()){
+                    Headgear_female.setVisibility(View.GONE);
+                    Outerwear_female.setVisibility(View.GONE);
+                    Underwear_female.setVisibility(View.VISIBLE);
+                    Shoes_female.setVisibility(View.GONE);
+
+                    Headgear_male.setVisibility(View.GONE);
+                    Outerwear_male.setVisibility(View.GONE);
+                    Underwear_male.setVisibility(View.GONE);
+                    Shoes_male.setVisibility(View.GONE);
+                }
+                break;
+            case "Shoes":
+                if(male.isChecked()) {
+                    Headgear_male.setVisibility(View.GONE);
+                    Outerwear_male.setVisibility(View.GONE);
+                    Underwear_male.setVisibility(View.GONE);
+                    Shoes_male.setVisibility(View.VISIBLE);
+
+                    Headgear_female.setVisibility(View.GONE);
+                    Outerwear_female.setVisibility(View.GONE);
+                    Underwear_female.setVisibility(View.GONE);
+                    Shoes_female.setVisibility(View.GONE);
+                }
+                else if(female.isChecked()){
+                    Headgear_female.setVisibility(View.GONE);
+                    Outerwear_female.setVisibility(View.GONE);
+                    Underwear_female.setVisibility(View.GONE);
+                    Shoes_female.setVisibility(View.VISIBLE);
+
+                    Headgear_male.setVisibility(View.GONE);
+                    Outerwear_male.setVisibility(View.GONE);
+                    Underwear_male.setVisibility(View.GONE);
+                    Shoes_male.setVisibility(View.GONE);
+                }
+                break;
         }
+        Place_an_order.setVisibility(View.VISIBLE);
     }
-    public String OutTypeCloth(int SelectedItemPosition){
-        String[] array = getResources().getStringArray(R.array.array_type_cloth);
-        return array[SelectedItemPosition];
+    public String OutTypeCloth(int SelectedItemPosition, int sel){
+        switch (sel){
+            case 1:
+                String[] array_cloth = getResources().getStringArray(R.array.array_type_cloth);
+                return array_cloth[SelectedItemPosition];
+            case 2:
+                String[] array_size = getResources().getStringArray(R.array.array_size);
+                return array_size[SelectedItemPosition];
+
+            case 3:
+                String[] array_male_head = getResources().getStringArray(R.array.array_Headgear_male);
+                return array_male_head[SelectedItemPosition];
+            case 4:
+                String[] array_male_out = getResources().getStringArray(R.array.array_Outerwear_male);
+                return array_male_out[SelectedItemPosition];
+            case 5:
+                String[] array_male_under = getResources().getStringArray(R.array.array_Underwear_male);
+                return array_male_under[SelectedItemPosition];
+            case 6:
+                String[] array_male_shoes = getResources().getStringArray(R.array.array_Shoes_male);
+                return array_male_shoes[SelectedItemPosition];
+
+            case 7:
+                String[] array_female_head = getResources().getStringArray(R.array.array_Headgear_female);
+                return array_female_head[SelectedItemPosition];
+            case 8:
+                String[] array_female_out = getResources().getStringArray(R.array.array_Outerwear_female);
+                return array_female_out[SelectedItemPosition];
+            case 9:
+                String[] array_female_under = getResources().getStringArray(R.array.array_Underwear_female);
+                return array_female_under[SelectedItemPosition];
+            case 10:
+                String[] array_female_shoes = getResources().getStringArray(R.array.array_Shoes_female);
+                return array_female_shoes[SelectedItemPosition];
+        }
+        return "";
+    }
+
+    public void PlaceAnOrder(View view) {
+        Intent intent = new Intent(this, Order_check.class);
+
+        intent.putExtra("login", login);
+        intent.putExtra("password", password);
+
+        String gender = "";
+        String cloth = "";
+
+        if(male.isChecked()){
+            gender = "Male";
+            switch (OutTypeCloth(select_type.getSelectedItemPosition(), 1)){
+                case "Headgear":
+                    cloth = OutTypeCloth(Headgear_male.getSelectedItemPosition(), 3);
+                    break;
+                case "Outerwear":
+                    cloth = OutTypeCloth(Outerwear_male.getSelectedItemPosition(), 4);
+                    break;
+                case "Underwear":
+                    cloth = OutTypeCloth(Underwear_male.getSelectedItemPosition(), 5);
+                    break;
+                case "Shoes":
+                    cloth = OutTypeCloth(Shoes_male.getSelectedItemPosition(), 6);
+                    break;
+            }
+        }
+        else if(female.isChecked()){
+            gender = "Female";
+            switch (OutTypeCloth(select_type.getSelectedItemPosition(), 1)){
+                case "Headgear":
+                    cloth = OutTypeCloth(Headgear_female.getSelectedItemPosition(), 7);
+                    break;
+                case "Outerwear":
+                    cloth = OutTypeCloth(Outerwear_female.getSelectedItemPosition(), 8);
+                    break;
+                case "Underwear":
+                    cloth = OutTypeCloth(Underwear_female.getSelectedItemPosition(), 9);
+                    break;
+                case "Shoes":
+                    cloth = OutTypeCloth(Shoes_female.getSelectedItemPosition(), 10);
+                    break;
+            }
+        }
+        intent.putExtra("gender", gender);
+        intent.putExtra("size", OutTypeCloth(select_size.getSelectedItemPosition(), 2));
+        intent.putExtra("type_cloth", OutTypeCloth(select_type.getSelectedItemPosition(), 1));
+        intent.putExtra("cloth", cloth);
+
+        startActivity(intent);
     }
 }
